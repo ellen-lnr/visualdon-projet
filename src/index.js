5eb5b0f82269d242d6caa729a867d168098bbf9c
Loriane Magnenat <loriane@MacBook-Pro-de-Loriane.local>
2025-04-17 15:58:49 +0200

    Visualisation carte

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// import { initCarteStations } from '../etapes/6_evacuation';
+
+// document.addEventListener('DOMContentLoaded', () => {
+//   const section6 = document.querySelector('.section6');
+//   if (section6) {
+//     initCarteStations();
+//   }
+// });
